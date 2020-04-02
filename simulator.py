a9eb6a18aa671043a2b3c4cec019c4802ff192e9
Axel Conceicao <axel.conceicao@epitech.eu>
2020-04-02 02:12:30 +0200

    simulator no longer need .sh, .py handles everything (merger, cleaner and engine)

--- simulator.py
+++ simulator.py
@@ -1,18 +1,26 @@
+import os.path
 import sys
 import misc
 import merger
-import splitter
+import cleaner
+import subprocess
 
 class Simulator:
     scriptfile = None
     testfile = None
-    gamesperfile = 50000
+    tempdir = ".temp"
+    options = []
 
-    def __init__(self, scriptfile, testfile):
+    def __init__(self, scriptfile, testfile, options):
+        self.options = options
         misc.isFileExist(scriptfile); self.scriptfile = scriptfile
         misc.isFileExist(testfile); self.testfile = testfile
-        merger.Merger(self.scriptfile, self.gamesperfile).merge()
-        splitter.Splitter(self.testfile, self.gamesperfile).split()
+        cleaner.Cleaner(self.testfile, self.tempdir).clean()
+        merger.Merger(self.scriptfile, self.tempdir, self.options).merge()
+
+    def start(self):
+        print("Starting...")
+        subprocess.run(["node", self.tempdir + "/" + "script.js", self.tempdir + "/test.txt"])
 
 if __name__ == "__main__":
     if "-h" in sys.argv:
@@ -21,5 +29,9 @@ if __name__ == "__main__":
     elif len(sys.argv) < 3:
         misc.printUsage()
         exit(1)
-    simulator = Simulator(sys.argv[1], sys.argv[2])
+    options = []
+    for av in sys.argv:
+        if av.lower() == '-l':
+            options.append(['-l', 0])
+    Simulator(sys.argv[1], sys.argv[2], options).start()
     exit(0)
